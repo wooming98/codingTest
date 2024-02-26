08b5950f3d799d5babc36c650a1649e38633a476
wooming98 <113446287+wooming98@users.noreply.github.com>
2024-02-26 16:57:57 +0900

    [level 0] Title: 커피 심부름, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        for(String s : order) {
+            if(s.contains("americano") || s.contains("anything")) {
+                answer += 4500;
+            } else {
+                answer += 5000;
+            }
+        }
+        return answer;
+    }
+}
