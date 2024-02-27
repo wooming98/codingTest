959eadecc6935fbedc69ec3673d85b5678d560d2
wooming98 <113446287+wooming98@users.noreply.github.com>
2024-02-27 11:01:50 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 67.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = String.valueOf(n);
+        for(int i=0; i<str.length(); i++) {
+            answer += str.charAt(i) -48;
+        }
+        return answer;
+    }
+}
