9521a56ea0484f72b964265ecde64cf9e3cbce38
wooming98 <113446287+wooming98@users.noreply.github.com>
2024-02-28 12:10:20 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int[] numbers) {
         int sum = 0;
-        for(int i=0; i<numbers.length; i++) {
-            sum += numbers[i];
+        for(int i : numbers) {
+            sum += i;
         }
         return 45 - sum;
     }
