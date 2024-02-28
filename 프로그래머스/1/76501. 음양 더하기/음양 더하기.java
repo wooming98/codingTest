751fb4cfcc82d5dec78ded2b3de9da59b19cda36
wooming98 <113446287+wooming98@users.noreply.github.com>
2024-02-28 11:27:45 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
         for(int i=0; i<signs.length; i++) {
-            answer += signs[i] == true ? absolutes[i] : -1 * absolutes[i];
+            answer += signs[i] == true ? absolutes[i] : -absolutes[i];
         }
         return answer;
     }
