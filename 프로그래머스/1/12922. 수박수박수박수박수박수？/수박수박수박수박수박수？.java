8eed45f83d25dc7a0decd22abd8df16c01eab6ce
wooming98 <113446287+wooming98@users.noreply.github.com>
2024-02-28 12:47:25 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 4.21 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "수박".repeat(n/2);
+        if(n % 2 != 0) {
+            answer += "수";
+        }
+        return answer;
+    }
+}
