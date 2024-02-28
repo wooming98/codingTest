c4261cfbffd7d0572589d55151121fb719641bed
wooming98 <113446287+wooming98@users.noreply.github.com>
2024-02-28 11:19:39 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,12 +1,11 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        int answer = 0;
-        if(a != b && b != c && a != c) {
-            answer = a + b + c;
-        } else if((a == b && a != c) || (a == c && b != c) || (b == c && a != c))  {
-            answer = (a + b + c) * (a*a+b*b+c*c);
-        } else {
-            answer = (a + b + c) * (a*a+b*b+c*c) * (a*a*a+b*b*b+c*c*c);
+        int answer = a+b+c;
+
+        if(a==b&&b==c){
+            answer = answer * (a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
+        }else if(a==b||a==c||b==c){
+            answer = answer * (a*a+b*b+c*c);
         }
         return answer;
     }
