19d6c4948ae6cf7080e8e93c8f4d1d0264a49c1f
wooming98 <113446287+wooming98@users.noreply.github.com>
2024-03-04 09:35:06 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.37 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        char[] array = my_string.toLowerCase().toCharArray();
+        Arrays.sort(array);
+        return new String(array);
+    }
+}
