e087dbd4411e282b1b0d9040a0f899f811fa9ea9
wooming98 <113446287+wooming98@users.noreply.github.com>
2024-03-11 16:23:39 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] str_list) {
+        List<String> list = new ArrayList<>();
+        for(int i=0; i<str_list.length; i++) {
+            if(str_list[i].equals("l")) {
+                for(int j=0; j<i; j++) {
+                    list.add(str_list[j]);
+                }
+                break;
+            } else if(str_list[i].equals("r")) {
+                for(int j=i+1; j<str_list.length; j++) {
+                    list.add(str_list[j]);
+                }
+                break;
+            }
+        }
+        String[] answer = new String[list.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
