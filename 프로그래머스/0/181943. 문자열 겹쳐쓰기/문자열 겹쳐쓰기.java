b7c46f424de297f76ea16fe0b79cf1459a5a5c4b
wooming98 <113446287+wooming98@users.noreply.github.com>
2024-03-12 14:59:26 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 10.03 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,13 +1,7 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        String str = "";
-        for(int i=0; i<my_string.length(); i++) {
-            if(i>=s && i<overwrite_string.length()+s) {
-                str += overwrite_string.charAt(i-s);
-            } else {
-                str += my_string.charAt(i);
-            }
-        }
-        return str;
+        String before = my_string.substring(0, s);
+        String after = my_string.substring(s + overwrite_string.length());
+        return before + overwrite_string + after;
     }
 }
