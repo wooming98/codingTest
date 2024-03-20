ed7c478506dffbe67665c0209f4e1a06bd28ea16
wooming98 <113446287+wooming98@users.noreply.github.com>
2024-03-20 17:38:24 +0900

    [level 0] Title: 문자열 밀기, Time: 10.72 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,5 +1,23 @@
+import java.util.*;
 class Solution {
     public int solution(String A, String B) {
-        return (B + B).indexOf(A);
+        if(A.equals(B)) {
+            return 0;
+        }
+        List<String> list = new ArrayList<>();
+        for(int i=0; i<A.length(); i++) {
+            list.add(""+A.charAt(i));
+        }
+        int idx = 0;
+        for(int i=0; i<list.size(); i++) {
+            if(!String.join("", list).equals(B)) {
+                list.add(0, list.get(list.size()-1));
+                list.remove(list.size()-1);
+                idx++;
+            } else {
+                return idx;
+            }
+        }
+        return -1;
     }
 }
