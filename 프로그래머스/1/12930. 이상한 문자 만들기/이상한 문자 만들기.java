6730658cff1f5dd6e9538fc33beaf3581486b57b
wooming98 <113446287+wooming98@users.noreply.github.com>
2024-03-25 17:35:29 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.08 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,22 +1,20 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        // 공백이 나오면 n=0으로 처리
-        // 문자가 나오면 n=0이 아닐 때, 대문자 소문자 ..로 처리
-        int n = -1;
-        for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) == ' ') {
-                n = -1;
-                answer += s.charAt(i);
+        char[] chars = s.toCharArray();
+        boolean tf = true;
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == ' ') {
+                tf = true;
             } else {
-                n++;
-                if(n % 2 == 0) {
-                    answer += Character.toUpperCase(s.charAt(i));
+                if (tf) {
+                    chars[i] = Character.toUpperCase(chars[i]);
                 } else {
-                    answer += Character.toLowerCase(s.charAt(i));
+                    chars[i] = Character.toLowerCase(chars[i]);
                 }
+                tf = !tf;
             }
         }
-        return answer;
+        return new String(chars);
     }
 }
