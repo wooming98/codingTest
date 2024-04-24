2645ac85734ff6982391383954dcb06817cc6ebd
wooming98 <113446287+wooming98@users.noreply.github.com>
2024-04-24 19:14:28 +0900

    [Bronze V] Title: A+B - 3, Time: 228 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        for(int i=0; i<n; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
