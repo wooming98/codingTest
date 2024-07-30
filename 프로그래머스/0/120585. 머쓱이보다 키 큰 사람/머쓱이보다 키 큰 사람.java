23b379fde100a73f29d12000cc764f55d6c8ff20
wooming98 <113446287+wooming98@users.noreply.github.com>
2024-07-30 12:02:26 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int count = 0;
+        for(int i : array) {
+            if(i > height) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
