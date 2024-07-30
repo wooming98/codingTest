7f27fd79aed4f02267115a88f1f83b006244637e
wooming98 <113446287+wooming98@users.noreply.github.com>
2024-07-30 12:33:18 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.23 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,10 +1,15 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] array) {
-        int answer = 0;
-        Arrays.sort(array);
-        answer = array[array.length/2];
+         for (int i=0; i<array.length; i++) {
+            for (int j=i; j<array.length; j++) {
+                if (array[j] < array[i]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        int answer = array[array.length/2];
         return answer;
     }
 }
